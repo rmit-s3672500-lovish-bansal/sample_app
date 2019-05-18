a44b900412500d5fe35a4ec1075433cda370fc7d
s3672500 <s3672500@student.rmit.edu.au>
2019-05-18 23:40:36 +0000

    I need a rest

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  
+  get 'about', to:'static_pages#about'  
+  get 'help', to:'static_pages#help'
+  root  'static_pages#home'
+  
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
